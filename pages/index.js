8f225f79f42ec64ff2072c8fe9d567912873ddba
kiborgok <borgokalek14@gmail.com>
2021-06-22 07:31:41 +0300

    Deleted a paragraph

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,6 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <h2 className={utilStyles.headingLg}>Description</h2>
         <p>@Software_Engineer dedicated at writing softwares.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
